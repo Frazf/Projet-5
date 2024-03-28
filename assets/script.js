3b22183d308547813f3cda6c9412d9ada73cefcb
Ahmed Fraz <frazahmed44@gmail.com>
2024-03-28 16:52:57 +0100

    Ajout commentaire pour se repérer sur le code

--- assets/script.js
+++ assets/script.js
@@ -17,23 +17,24 @@ const slides = [
 	}
 ]
 
-let arrow_left = document.querySelector(".arrow_left")
-const arrow_right = document.querySelector(".arrow_right")
-let index = 0;
+let arrow_left = document.querySelector(".arrow_left") // Sélectionne l'élément HTML avec la classe "arrow_left" et le stocke dans la variable arrow_left
+const arrow_right = document.querySelector(".arrow_right") // Sélectionne l'élément HTML avec la classe "arrow_right" et le stocke dans la constante arrow_right
+let index = 0; // Initialise la variable index à 0, qui servira à suivre l'index actuel des slides
 
 // Flèche 
 
+// Ajoute un écouteur d'événements sur le clic de la flèche droite
 arrow_right.addEventListener("click",function(){
-	index++;
-	if(index >= slides.length ){
-		index = 0;
+	index++; // Incrémente l'index
+	if(index >= slides.length ){ // Vérifie si l'index dépasse le nombre de slides
+		index = 0; // Réinitialise l'index à 0 s'il dépasse
 	}
-	console.log("Index est : " + index);
-	displayDots(index);
+	console.log("Index est : " + index); // Affiche l'index actuel dans la console
+	displayDots(index); 
 })
-
+// Ajoute un écouteur d'événements sur le clic de la flèche gauche
 arrow_left.addEventListener("click",function(){
-	index--;
+	index--; // Décrémente l'index 
 	if(index < 0){
 		index = slides.length -1;
 	}
